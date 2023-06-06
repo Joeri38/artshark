4567d6985fd07b98d136e5130616b9e89e163663
faixanmalik <faixan.malik554@gmail.com>
2023-06-06 13:41:36 +0500

    add product added form api model

--- src/pages/api/addproducts.js
+++ src/pages/api/addproducts.js
@@ -3,18 +3,17 @@ import connectDb from '../../../middleware/mongoose'
 
 const handler = async (req,res)=>{
     if (req.method == 'POST'){
+        
         let addProduct = new Product({
             title: req.body.title,
             desc: req.body.desc,
             slug: req.body.slug ,
-            img: req.body.img,
+            img1: req.body.img1,
+            img2: req.body.img2,
+            img3: req.body.img3,
             category: req.body.category,
-            size: req.body.size ,
-            color: req.body.color ,
             price: req.body.price,
-            avilableQty: req.body.avilableQty,
-            file: req.body.file,
-            })
+        })
         await addProduct.save();
         res.status(200).json({ success: "Item added succesfully!" })
     }
