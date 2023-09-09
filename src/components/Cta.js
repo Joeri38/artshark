231b9ce1ff2a34c500b75393a0b195a9edf76a5f
Joeri Lenaerts <joeri.lenaerts36@gmail.com>
2023-09-09 13:23:08 +0200

    change layout

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -7,11 +7,11 @@ function Cta () {
     <div className='relative'>
 
       <div className='absolute z-10 py-5 top-20 sm:top-44 md:top-40 md:left-36 xl:left-44 md:w-2/3 md:ml-10'>
-        <h1 className='text-3xl sm:text-4xl mb-14 font-sans tracking-wide text-white text-center font-semibold'>What technology are we using?</h1>
+        <h1 className='text-3xl sm:text-4xl mt-0 mb-14 font-sans tracking-wide text-blue-800 text-center font-semibold'>What technology are we using?</h1>
         <p className=' md:w-10/12 mx-auto text-sm tracking-tighter sm:text-base px-10 md:px-0 md:text-xl my-4 mb-7 text-center text-gray-200'>In a world buzzing with computing power and cutting-edge AI, we're riding the wave of generative artistry. Imagine AI conjuring up text, spouting speech, and crafting images that are mind-bogglingly lifelike or pure artistic magic. Well, guess what? We're inviting you to hop on this AI rollercoaster and wear the future, one design at a time!</p>
       </div>
       <div className=''>
-        <img className='w-full md:w-11/12 mx-auto h-[400px] sm:h-[500px] object-cover object-bottom rounded-md' src="https://images.unsplash.com/photo-1631755218195-8d8e7b2c04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjM1fHxkYXJrJTIwZGVza3xlbnwwfDB8MHx8&auto=format&fit=crop&w=600&q=60" alt="" />
+        <img className='w-full md:w-11/12 mx-auto h-[400px] sm:h-[500px] object-cover object-bottom rounded-md' src="images/home_beach.png" alt="" />
       </div>
 
     </div>
--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router'
 import Product from '../../../models/Product';
@@ -7,28 +7,22 @@ import mongoose from "mongoose";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-
 // React Toastify
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
-
-
 const Item = ({addToCart , product}) => {
   const router = useRouter()
-  //const { slug } = router.query
 
   const [color, setColor] = useState('')
   const [size, setSize] = useState('')
-  const [whatDoYouWant, setWhatDoYouWant] = useState('')
 
-  let colorArray = ["White", "Black", "Blue", "Red", "Green", "Yellow", "Orange"];
+  let colorArray = ["White", "Black"];
   let sizeArray = ["XS", "S", "M", "L", "XL", "XXL"];
 
-  // React tostify
-  const addcart = () => toast.success("Item is added in your Cart.!");
-
+  // React toastify
+  const toastItemAdded = () => toast.success("Item is added in your Cart!");
 
   return <>
     <Head>
@@ -59,43 +53,6 @@ const Item = ({addToCart , product}) => {
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">Art Shark</h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.title}</h1>
-            {/* <div className="flex">
-              <span className="flex items-center">
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-gray-500" viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
-              </span>
-              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                <a className="text-gray-500">
-                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
-            </div> */}
 
             <div className='py-3'>
               <FormControl variant="standard" sx={{ minWidth: 120 }}>
@@ -130,7 +87,6 @@ const Item = ({addToCart , product}) => {
                 </Select>
               </FormControl>
 
-              <TextField value={whatDoYouWant} onChange={(e)=> setWhatDoYouWant(e.target.value)} className='' multiline fullWidth id="standard-basic" label="What do you want!" variant="standard" />
             </div>
 
 
@@ -138,7 +94,7 @@ const Item = ({addToCart , product}) => {
             
             <div className="flex mt-5">
               <span className="title-font font-medium text-2xl text-gray-900">€{product.price}</span>
-              <button onClick={()=>{ addcart() , addToCart(product._id, product.title, 1 , product.price, product.img1, product.stripePriceId, size, color, whatDoYouWant )}} className="flex -mt-1 ml-auto bg-[#29D0d1] hover:bg-[#44B0B7] text-white rounded-xl font-semibold border-0 py-3 px-6 focus:outline-none text-sm md:text-base">Add to Cart</button>
+              <button onClick={()=>{ toastItemAdded(), addToCart(product._id, product.title, product.stripePriceId, product.price, product.img1,  size, color, 1)}} className="flex -mt-1 ml-auto bg-[#29D0d1] hover:bg-[#44B0B7] text-white rounded-xl font-semibold border-0 py-3 px-6 focus:outline-none text-sm md:text-base">Add to Cart</button>
               <ToastContainer position="bottom-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light"/>
             
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -158,11 +114,14 @@ const Item = ({addToCart , product}) => {
 
 
 export async function getServerSideProps(context) {
+
+  // Connect if needed
   if (!mongoose.connections[0].readyState){
     mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGO_URI)
   }
 
+  // Retrieve Product from mongoDB
   let product = await Product.findOne({_id: context.query.id})
     
   // Pass data to the page via props
