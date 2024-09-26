9fd6d0951c3ee5b6f534098e2c66080f11761e27
Joeri Lenaerts <joeri.lenaerts36@gmail.com>
2024-09-26 17:03:30 +0200

    change code to openai v4

--- src/pages/api/generate-image.js
+++ src/pages/api/generate-image.js
@@ -1,13 +1,14 @@
+// openai v3
 //import { Configuration, OpenAIApi } from "openai"
 
 //const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 //const openai = new OpenAIApi(configuration)
 
-// openai version ^4.0.0
+// openai v4
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env['OPENAI_API_KEY'],
 });
 
 export default async function handler(req, res) {
@@ -19,15 +20,22 @@ export default async function handler(req, res) {
 
         try {
             
-            // Send prompt to OpenAI API
-            const response = await openai.createImage({
+            // openai v3
+            /*const response = await openai.createImage({
                 model: "dall-e-3",
                 prompt: prompt,
-              });
+            });*/
+
+            // Send prompt to OpenAI API
+            // openai version ^4.0.0
+            const response = await openai.images.generate({
+                prompt: prompt,
+                model: "dall-e-3",
+            });
 
             // Get data from response
-            const image_url = response.data.data[0].url;
-            const revised_prompt = response.data.data[0].revised_prompt;
+            const image_url = response.data[0].url;
+            const revised_prompt = response.data[0].revised_prompt;
 
             // Send data to client
             res.status(200).json({ message: "Image generated!", 
