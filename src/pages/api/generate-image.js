66e5b0ae1c2243171658a9a329b4081c521fde99
Joeri Lenaerts <joeri.lenaerts36@gmail.com>
2024-09-26 16:40:40 +0200

    fix typo

--- src/pages/api/generate-image.js
+++ src/pages/api/generate-image.js
@@ -7,7 +7,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default async function handler(req, res) {
